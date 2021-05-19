2247b08a7fee2b6ad66f93cc83c055c90fa31bc0
venue <82924899+ansh709@users.noreply.github.com>
2021-05-19 21:52:41 +0530

    Add files via upload

---
+++ js/homepage.js
@@ -0,0 +1,32 @@
+class Process {
+    constructor(img) {
+        this.img = img;
+    }
+}
+
+var process = new Process("images/process.png");
+
+processData = () => {
+
+    document.getElementById('process').innerHTML += `
+<div class="container-fluid">
+   <div class="row">
+       <div class="col-md-12">
+           <div class="processItem">
+              <img src="${process.img}">
+           </div>
+       </div>
+   </div>
+</div>
+`;
+}
+document.addEventListener("DOMContentLoaded", processData);
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+        navigator.serviceWorker
+            .register("serviceWorker.js")
+            .then(res => console.log("service worker registered"))
+            .catch(err => console.log("service worker not registered", err));
+    });
+}
